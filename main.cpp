82af562e97d8606194b85dc0e02908e698a8b6f0
BtbN <btbn@btbn.de>
2014-11-20 13:39:31 +0100

    test

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <QtDebug>
+
 #include <QApplication>
 #include <QtSpeech>
 
@@ -7,7 +9,15 @@ int main(int argc, char *argv[])
 	QApplication app(argc, argv);
 
 	QtSpeech speech;
-	speech.tell("TESTING");
 
+	for(QtSpeech::VoiceName name: speech.voices())
+	{
+		qDebug() << name.name;
+	}
+
+	speech.say("TESTING THE PLAYBACK SOUND SYSTEM");
+	speech.tell("TESTING THE PLAYBACK SOUND SYSTEM");
+
+	return 0;
 	return app.exec();
 }
